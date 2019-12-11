7793b28fdf6be85bab6c62beda8b19fa14269fda
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-11 19:54:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i=0; i<array.length; i++)
+  {
+    if(array[i] === stopValue) {
+      break
+    }
+
+    array[i] = changeValue
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  
+}
+
+function findBy(array, findFn) {
+  
+}
